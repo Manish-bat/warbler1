0f2e9aa3cda1e9506645c4e7ccf0ebb9309c4f88
Manish-bat <manishyadave800@gmail.com>
2020-11-05 12:40:47 +0530

    initial

---
+++ warbler-server/index.js
@@ -0,0 +1,52 @@
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+const cors = require("cors");
+const errorHandler = require("./handlers/error");
+const authRoutes  = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
+
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+app.use(
+	"/api/users/:id/messages",
+    loginRequired,
+    ensureCorrectUser, 
+    messageRoutes
+    );
+// all my routes here - they will come after
+
+app.get("/api/messages", loginRequired, async function(req, res, next){
+	try {
+		let messages = await db.message.find()
+		.sort({createdAt: "desc" })
+		.populate("user", {
+			username: true,
+			profileImageUrl: true
+		});
+		return res.status(200).json(messages);
+	} catch (err) {
+		return next(err);
+	}
+})
+
+app.use(function(req, res, next) {
+	let err = new Error("Not Found");
+	err.status = 404;
+	next(err); 
+});
+
+app.use(errorHandler);
+
+
+app.listen(PORT, function() {
+	console.log("Server is starting on port ${PORT}")
+})
---
+++ warbler-server/routes/messages.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+
+const { createMessage } = require("../handlers/messages");
+
+// prefix - /api/users/:id/messages
+
+router.route("/").post(createMessage);
+
+module.exports = router;
