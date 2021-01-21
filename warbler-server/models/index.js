b301af846205cf54d143eeacd0eb2f4a90f61efd
Manish-bat <manishyadave800@gmail.com>
2021-01-21 11:14:44 +0530

    commit

--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", {
-	keepAlive: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/warbler", {
+	keepAlive: true,
+useUnifiedTopology: true,
+	useCreateIndex: true,
+useNewUrlParser: true
 });
 
 
--- warbler-server/routes/auth.js
+++ warbler-server/routes/auth.js
@@ -5,5 +5,4 @@ const { signup, signin } = require("../handlers/auth");
 router.post("/signup", signup);
 router.post("/signin", signin);
 
-
 module.exports = router;
